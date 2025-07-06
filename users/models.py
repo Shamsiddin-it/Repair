d6431bdfb0cb2c9a230613ea47cd55c4817def70
Shamsiddin-it <samsiddinarbobov@gmail.com>
2025-07-06 10:09:52 +0500

    added str to WorkerProfile and User

--- users/models.py
+++ users/models.py
@@ -5,6 +5,9 @@ class User(AbstractUser):
     is_client = models.BooleanField(default=False)
     is_worker = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.username
+
 class WorkerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     skills = models.TextField()
@@ -12,6 +15,8 @@ class WorkerProfile(models.Model):
     hourly_rate = models.DecimalField(max_digits=10, decimal_places=2)
     location = models.CharField(max_length=255)
     is_available = models.BooleanField(default=True)
+    def __str__(self):
+        return self.user
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
