cfb6c322e18a435404a7a4e2c0485e333bc9812f
Shamsiddin-it <samsiddinarbobov@gmail.com>
2025-07-11 12:24:31 +0500

    3000 host

--- server/settings.py
+++ server/settings.py
@@ -155,7 +155,7 @@ SWAGGER_SETTINGS = {
 
 ORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://localhost:5173",
+    "http://localhost:3000",
     "http://127.0.0.1:5173",
     "http://147.45.146.15:1001",
 ]
